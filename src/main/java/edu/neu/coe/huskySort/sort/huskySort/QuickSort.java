779dabe4ee1b4faf40000593fa9575d7268a24fa
Sabbbbber <katecatkitten@gmail.com>
2022-08-04 20:50:01 -0400

    update test and benchmark for quick sort

--- src/main/java/edu/neu/coe/huskySort/sort/huskySort/QuickSort.java
+++ src/main/java/edu/neu/coe/huskySort/sort/huskySort/QuickSort.java
@@ -56,60 +56,94 @@ public class QuickSort<X extends Comparable<X>> extends AbstractHuskySort<X>  {
      * @param to   the index of the first element not to sort.
      */
     public void sort(final X[] xs, final int from, final int to) {
-        quickSort(xs, getHelper().getLongs(), from, to - 1);
+        quickSort(xs, from, to - 1);
     }
 
     // CONSIDER inlining this private method
     // CONSIDER redefining to to be one higher.
     @SuppressWarnings({"UnnecessaryLocalVariable"})
-    private void quickSort(final X[] objects, final long[] longs, final int from, final int to) {
+    private void quickSort(final X[] objects,  final int from, final int to) {
+
+
         final int lo = from;
         final int hi = to;
         if (hi <= lo) return;
         int j = partition(objects, lo, hi);
         sort(objects, lo, j-1);
         sort(objects, j+1, hi);
+
+         /*
+        if (from < to)
+        {
+
+            // pi is partitioning index, arr[p]
+            // is now at right place
+            int pi = partition(objects, from, to);
+
+            // Separately sort elements before
+            // partition and after partition
+            quickSort(objects, from, pi - 1);
+            quickSort(objects, pi + 1, to);
+        }
+
+          */
     }
 
     private int partition(final X[] objects,  final int lo, final int hi) {
 
         int i = lo;
-        int j = hi + 1;
+        int j = hi;
+        int pivot = lo;
         while (true) {
 
-            // find item on lo to swap
-            while (compare(objects, ++i, lo) < 0 ) {
-                if (i == hi) break;
+            //lo id the pivot
+            // swap item on lo
+            while (compare(objects, ++i, pivot) < 0) {
+                if (i == hi - 1) break;
             }
 
-            // find item on hi to swap
-            while (compare(objects, lo, --j) < 0) {
-                if (j == lo) break;      // redundant since a[lo] acts as sentinel
+            // find item to swap
+            while (compare(objects, pivot, --j) < 0) {
+                if (j == lo) break;
             }
 
-            // check if pointers cross
+            // check if i and j cross
             if (i >= j) break;
 
             swap(objects, i, j);
         }
 
-        // put partitioning item v at a[j]
+        // partitioning item lo at j
         swap(objects, lo, j);
 
-        // now, a[lo .. j-1] <= a[j] <= a[j+1 .. hi]
         return j;
-    }
+        /*
+        int pivot = hi;
 
+        // Index of smaller element and
+        // indicates the right position
+        // of pivot found so far
+        int i = (lo - 1);
 
+        for(int j = lo; j <= hi - 1; j++)
+        {
 
-    private static class Partition {
-        final int lt;
-        final int gt;
+            // If current element is smaller
+            // than the pivot
+            if (compare(objects, j,  pivot)< 0)
+            {
 
-        Partition(final int lt, final int gt) {
-            this.lt = lt;
-            this.gt = gt;
+                // Increment index of
+                // smaller element
+                i++;
+                swap(objects, i, j);
+            }
         }
+        swap(objects, i + 1, hi);
+        return (i + 1);
     }
+*/
 
+
+    }
 }
--- src/test/java/edu/neu/coe/huskySort/sort/huskySort/QuickSortTest.java
+++ src/test/java/edu/neu/coe/huskySort/sort/huskySort/QuickSortTest.java
@@ -14,9 +14,7 @@ import java.io.IOException;
 import java.math.BigInteger;
 import java.time.LocalDateTime;
 import java.time.chrono.ChronoLocalDateTime;
-import java.util.Date;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 import java.util.regex.Pattern;
 
 import static edu.neu.coe.huskySort.sort.huskySortUtils.HuskySortHelper.generateRandomAlphaBetaArray;
@@ -26,90 +24,101 @@ import static org.junit.Assert.assertTrue;
 @SuppressWarnings("MagicConstant")
 public class QuickSortTest {
 
+    @Test
+    public void testSortString() {
 
-    @Test
-    public void testSortTryBySab() {
-            final Config config = ConfigTest.setupConfig("true", "0", "1", "", "");
-            final QuickSort<String> sorter = new QuickSort<>(HuskyCoderFactory.asciiCoder, config);
-            final HuskyHelper<String> helper = sorter.getHelper();
-            int N = 1000;
-            helper.init(N);
-            final String[] xs = helper.random(String.class, r -> r.nextLong() + "");
-            final int inversionsOriginal = helper.inversions(xs);
-            System.out.println("inversions: " + inversionsOriginal);
-            sorter.preProcess(xs);
-            final String[] ys = sorter.sort(xs);
-            sorter.postProcess(ys);
-            final ComparisonSortHelper<String> delegateHelper = InstrumentedComparisonSortHelper.getInstrumentedHelper(helper, (InstrumentedComparisonSortHelper<String>) helper.getHelper());
-            final PrivateMethodInvoker privateMethodInvoker = new PrivateMethodInvoker(delegateHelper);
-            final StatPack statPack = (StatPack) privateMethodInvoker.invokePrivate("getStatPack");
-            System.out.println(statPack);
-
-    }
-    @Test
-    public void testQuickSort() {
-        for (int i = 100; i <= 1000; i= i*10) {
-            final Config config = ConfigTest.setupConfig("true", "0", "1", "", "");
-            final QuickSort<String> sorter = new QuickSort<>(HuskyCoderFactory.asciiCoder, config);
-            final HuskyHelper<String> helper = sorter.getHelper();
-            int N = i;
-            helper.init(N);
-            final String[] xs = helper.random(String.class, r -> r.nextLong() + "");
-            final int inversionsOriginal = helper.inversions(xs);
-            System.out.println("N: " + N);
-            System.out.println("inversions: " + inversionsOriginal);
-            sorter.preProcess(xs);
-            final String[] ys = sorter.sort(xs);
-            sorter.postProcess(ys);
-            final ComparisonSortHelper<String> delegateHelper = InstrumentedComparisonSortHelper.getInstrumentedHelper(helper, (InstrumentedComparisonSortHelper<String>) helper.getHelper());
-            final PrivateMethodInvoker privateMethodInvoker = new PrivateMethodInvoker(delegateHelper);
-            final StatPack statPack = (StatPack) privateMethodInvoker.invokePrivate("getStatPack");
-            System.out.println(statPack);
-        }
-    }
-    @Test
-    public void testSortString2() {
-        final Config config = ConfigTest.setupConfig("true", "0", "1", "", "");
-        final QuickSort<String> sorter = new QuickSort<>(HuskyCoderFactory.asciiCoder, config);
-        final HuskyHelper<String> helper = sorter.getHelper();
-        final int N = 1000;
-        helper.init(N);
-        final String[] xs = helper.random(String.class, r -> r.nextLong() + "");
-        System.out.println("N: " + N);
-        sorter.preProcess(xs);
-        final String[] ys = sorter.sort(xs);
-        sorter.postProcess(ys);
-        final ComparisonSortHelper<String> delegateHelper = InstrumentedComparisonSortHelper.getInstrumentedHelper(helper, (InstrumentedComparisonSortHelper<String>) helper.getHelper());
-        final PrivateMethodInvoker privateMethodInvoker = new PrivateMethodInvoker(delegateHelper);
-        final StatPack statPack = (StatPack) privateMethodInvoker.invokePrivate("getStatPack");
-        System.out.println(statPack);
-    }
-
-    @Test
-    public void testSortString3() {
         final Config config = ConfigTest.setupConfig("true", "0", "1", "", "true");
         final QuickSort<String> sorter = new QuickSort<>(HuskyCoderFactory.asciiCoder, config);
         final HuskyHelper<String> helper = sorter.getHelper();
-        final int N = 1000;
+        final int N = 30;
         helper.init(N);
         final String[] xs = helper.random(String.class, r -> {
             final int x = r.nextInt(1000000000);
             final BigInteger b = BigInteger.valueOf(x).multiply(BigInteger.valueOf(1000000));
             return b.toString();
         });
+        //shuffle again
+        List<String> strList = Arrays.asList(xs);
+        Collections.shuffle(strList);
+        String xs1[] = strList.toArray(new String[strList.size()]);
+        final int inversionsOriginal = helper.inversions(xs1);
+        System.out.println("inversions: " + inversionsOriginal);
+
+        sorter.preProcess(xs1);
+        final String[] ys = sorter.sort(xs1);
+        assertTrue("sorted", helper.sorted(xs1));
+        sorter.postProcess(ys);
+
+
+        final ComparisonSortHelper<String> delegateHelper = InstrumentedComparisonSortHelper.getInstrumentedHelper(helper, (InstrumentedComparisonSortHelper<String>) helper.getHelper());
+        final PrivateMethodInvoker privateMethodInvoker = new PrivateMethodInvoker(delegateHelper);
+        final StatPack statPack = (StatPack) privateMethodInvoker.invokePrivate("getStatPack");
+        System.out.println(statPack);
+        //test if the string sorted
+        int i = (int) (Math.random() * N) % N;
+        String test1 = ys[i];
+        String test2 = ys[i+1];
+        assertTrue(test1.charAt(0) <= test2.charAt(0));
+    }
+
+    @Test
+    public void testSortString1() {
+        final Config config = ConfigTest.setupConfig("true", "0", "1", "", "");
+        final QuickSort<String> sorter = new QuickSort<>(HuskyCoderFactory.asciiCoder, config);
+        final HuskyHelper<String> helper = sorter.getHelper();
+        final int N = 1001;
+        helper.init(N);
+        final String[] xs = helper.random(String.class, r -> r.nextLong() + "");
         final int inversionsOriginal = helper.inversions(xs);
         System.out.println("inversions: " + inversionsOriginal);
         sorter.preProcess(xs);
         final String[] ys = sorter.sort(xs);
+        assertTrue("sorted", helper.sorted(ys));
         sorter.postProcess(ys);
         final ComparisonSortHelper<String> delegateHelper = InstrumentedComparisonSortHelper.getInstrumentedHelper(helper, (InstrumentedComparisonSortHelper<String>) helper.getHelper());
         final PrivateMethodInvoker privateMethodInvoker = new PrivateMethodInvoker(delegateHelper);
         final StatPack statPack = (StatPack) privateMethodInvoker.invokePrivate("getStatPack");
         System.out.println(statPack);
+        //test if the string sorted
+        int i = (int) (Math.random() * N) % N;
+        String test1 = ys[i];
+        String test2 = ys[i + 1];
+        assertTrue(test1.charAt(0) <= test2.charAt(0));
     }
 
 
+    @Test
+    public void benchmarkQuickSort() {
 
+        for (int i = 1000; i < 50000; i = i * 2) {
+            for (int k = 0; k < 5; k++){
+                final Config config = ConfigTest.setupConfig("true", "0", "1", "", "");
+                final QuickSort<String> sorter = new QuickSort<>(HuskyCoderFactory.asciiCoder, config);
+                final HuskyHelper<String> helper = sorter.getHelper();
+                int N = i;
+                helper.init(N);
+                final String[] xs = helper.random(String.class, r -> {
+                    final int x = r.nextInt(1000000000);
+                    final BigInteger b = BigInteger.valueOf(x).multiply(BigInteger.valueOf(100000000));
+                    return b.toString();
+                });
+                List<String> strList = Arrays.asList(xs);
+                Collections.shuffle(strList);
+                String[] xs1 = strList.toArray(new String[N]);
+                System.out.println("N: " + N );
+                sorter.preProcess(xs1);
+                final String[] ys = sorter.sort(xs1);
+                sorter.postProcess(ys);
+                final ComparisonSortHelper<String> delegateHelper = InstrumentedComparisonSortHelper.getInstrumentedHelper(helper, (InstrumentedComparisonSortHelper<String>) helper.getHelper());
+                final PrivateMethodInvoker privateMethodInvoker = new PrivateMethodInvoker(delegateHelper);
+                final StatPack statPack = (StatPack) privateMethodInvoker.invokePrivate("getStatPack");
+                final int compares = (int) statPack.getStatistics(Instrumenter.COMPARES).mean();
+                final int swaps = (int) statPack.getStatistics(Instrumenter.SWAPS).mean();
+                System.out.println("compares: " + compares + " swaps: " + swaps);
+
+            }
+        }
+    }
     @BeforeClass
     public static void before() throws IOException {
         config = Config.load(HuskySortTest.class);
