86b790aefc2d4b9d772d4fff2bdaa32d44c609c8
Sabbbbber <katecatkitten@gmail.com>
2022-08-09 03:56:07 -0400

    update qusort and its benchmark

--- src/main/java/edu/neu/coe/huskySort/sort/huskySort/QuickSort.java
+++ src/main/java/edu/neu/coe/huskySort/sort/huskySort/QuickSort.java
@@ -59,91 +59,35 @@ public class QuickSort<X extends Comparable<X>> extends AbstractHuskySort<X>  {
         quickSort(xs, from, to - 1);
     }
 
-    // CONSIDER inlining this private method
-    // CONSIDER redefining to to be one higher.
-    @SuppressWarnings({"UnnecessaryLocalVariable"})
     private void quickSort(final X[] objects,  final int from, final int to) {
-
-
         final int lo = from;
         final int hi = to;
         if (hi <= lo) return;
         int j = partition(objects, lo, hi);
-        sort(objects, lo, j-1);
-        sort(objects, j+1, hi);
-
-         /*
-        if (from < to)
-        {
-
-            // pi is partitioning index, arr[p]
-            // is now at right place
-            int pi = partition(objects, from, to);
-
-            // Separately sort elements before
-            // partition and after partition
-            quickSort(objects, from, pi - 1);
-            quickSort(objects, pi + 1, to);
-        }
-
-          */
+        quickSort(objects, lo, j-1);
+        quickSort(objects, j+1, hi);
     }
 
     private int partition(final X[] objects,  final int lo, final int hi) {
-
         int i = lo;
         int j = hi;
         int pivot = lo;
         while (true) {
-
             //lo id the pivot
             // swap item on lo
-            while (compare(objects, ++i, pivot) < 0) {
+            while (compare(objects, i++, pivot) < 0) {
                 if (i == hi - 1) break;
             }
-
             // find item to swap
             while (compare(objects, pivot, --j) < 0) {
                 if (j == lo) break;
             }
-
             // check if i and j cross
             if (i >= j) break;
-
             swap(objects, i, j);
         }
-
         // partitioning item lo at j
         swap(objects, lo, j);
-
         return j;
-        /*
-        int pivot = hi;
-
-        // Index of smaller element and
-        // indicates the right position
-        // of pivot found so far
-        int i = (lo - 1);
-
-        for(int j = lo; j <= hi - 1; j++)
-        {
-
-            // If current element is smaller
-            // than the pivot
-            if (compare(objects, j,  pivot)< 0)
-            {
-
-                // Increment index of
-                // smaller element
-                i++;
-                swap(objects, i, j);
-            }
-        }
-        swap(objects, i + 1, hi);
-        return (i + 1);
-    }
-*/
-
-
     }
 }
