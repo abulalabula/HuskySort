8faea119637c355368c3435375fdb0171b010924
abulalabula <abulalabula@gmail.com>
2022-08-08 23:16:31 -0400

    Update random and size method and fix bugs

--- src/main/java/edu/neu/coe/huskySort/bst/BSTValidation.java
+++ src/main/java/edu/neu/coe/huskySort/bst/BSTValidation.java
@@ -4,74 +4,82 @@ import java.util.*;
 
 public class BSTValidation {
 
-    public static void BSTHeight(int count, int numberOfDeletion, int runningTimes){
+
+    public static void BSTHeight(int count, int numberOfDeletion, int runningTimes, String deleteType, int maxDigit){
 
         Random random = new Random();
         for (int r = 0; r < runningTimes; r++) {
-
-            int totalNodes = 0;
-            ArrayList<Integer> allNodes = new ArrayList<>();
-            BSTSimple<String, Integer> bst = new BSTSimple<>();
-
+            ArrayList<String> allNodes = new ArrayList<>();
+            BSTOriginal<String, Integer> bst = new BSTOriginal<>();
             // Add nodes
-            for (int i = 0; i < count; i++) {
-                int randInteger = random.nextInt(count) + 1;
-                String randString = String.valueOf(randInteger);
-//                System.out.println("randString: " + randString + " randInteger: " + randInteger);
+            while (bst.size() < count){
+                int randInteger = random.nextInt(count);
+                String randString = String.format("%0"+ maxDigit +"d", randInteger);
                 bst.put(randString, randInteger);
-                // keep array uniqueness
-//                if (!allNodes.contains(randInteger)){
-//                    allNodes.add(randInteger);
-//                }
-                allNodes.add(randInteger);
+                // keep node array uniqueness
+                if (!allNodes.contains(randString)){
+                    allNodes.add(randString);
+                }
             }
-            totalNodes = allNodes.size();
-
             // Delete nodes
             for (int i = 0; i < numberOfDeletion; i++) {
                 if (allNodes.size() <= 0) break;
-//                int randDelete = random.nextInt(allNodes.size());
-//                String randString = Integer.toString(allNodes.get(randDelete));
-                int randDelete = random.nextInt(count) + 1;
-                String randString = String.valueOf(randDelete);
+                int randDelete = random.nextInt(count) ;
+                String randString = String.format("%0"+ maxDigit +"d", randDelete);
+                // type of deletion
+                if (deleteType == "random"){
+                    int direction = random.nextInt(2);
+                    if(direction == 0){
+                        bst.delete(randString);
+                    } else bst.deleteLeft(randString);
+                } else if (deleteType == "size"){
 
-//                System.out.println("randDelete: " + randDelete + ", randString: " + randString);
+                    int leftSize = 0;
+                    int rightSize = 0;
+                    leftSize = ((bst.root.left == null) ? 0 : bst.root.left.size);
+                    rightSize = ((bst.root.right == null) ? 0 : bst.root.right.size);
 
-                bst.delete(randString);
+                    if(leftSize > rightSize) bst.deleteLeft(randString);
+                    else if (leftSize < rightSize) bst.delete(randString);
+                    else {
+                        int direction = random.nextInt(2);
+                        if(direction == 0){
+                            bst.delete(randString);
+                        } else bst.deleteLeft(randString);
+                    }
+                } else {
+                    bst.delete(randString);
+                }
 
-                if( allNodes.size() > randDelete){
-                    allNodes.remove(randDelete);
+                // Check deletion
+                if(allNodes.contains(randString)){
+                    allNodes.remove(randString);
+
+                    // Add a node back
+                    int randSupplement = random.nextInt(count);
+                    String randSupplementString = String.format("%0"+ maxDigit +"d", randSupplement);
+                    while(allNodes.contains(randSupplementString)){
+                        randSupplement = random.nextInt(count);
+                        randSupplementString = String.format("%0"+ maxDigit +"d", randSupplement);
+                    }
+                    bst.put(randSupplementString, randSupplement);
+                    allNodes.add(randSupplementString);
+
                 } else continue;
-
-//                if(allNodes.contains(randDelete)){
-//                    System.out.println("node to delete: " + randDelete);
-//                    allNodes.remove(randDelete);
-//                    System.out.println("allnodes delete: " + allNodes);
-//                }
-
-                int randSupplement = random.nextInt(count);
-//                System.out.println("put back: " + randSupplement);
-                bst.put(Integer.toString(randSupplement), randSupplement);
-//                if (!allNodes.contains(randSupplement)){
-//                    allNodes.add(randSupplement);
-//                }
-                allNodes.add(randSupplement);
             }
-            System.out.println("n: " + totalNodes + ", delete size: "+ (totalNodes - allNodes.size()) +",rootOfn: " + Math.sqrt(totalNodes) + ",TreeHeight: " + bst.depth());
-
+            System.out.println(deleteType + "," + bst.size() + "," + Math.sqrt(bst.size()) + "," + bst.height());
         }
-
-
     }
+
+    private static void construct(int nodesNumber, int maxDigit){}
+    private static void hibbardDelete(int nodesNumber, int numberOfDeletion, int maxDigit, String deleteType){}
 
     public static void main(String[] args) {
 
-        for (int i = 5; i <= 2050; i*=2) {
+        for (int i = 25; i <= 6400; i*=2) {
+            System.out.println("");
             System.out.println("i: " + i);
-            BSTHeight(i, i/2, 10);
-            System.out.println("--------------------");
+            BSTHeight(i, i/2, 20, "random", 4);
         }
-
-
     }
 }
